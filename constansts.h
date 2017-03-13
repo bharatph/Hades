b224e46f6e1766b562b22b75687506a339075980
Bharatvaj <bharatvaj@yahoo.com>
2017-03-13 01:15:29 -0400

    Create constansts.h
    constant.h initial commit

---
+++ constansts.h
@@ -0,0 +1,27 @@
+#ifndef CONS_H
+#define CONS_H
+
+static enum folks {
+  USER,
+  POLICE,
+  ALL
+}
+
+static const char * authors_name = {
+  "Bharatvaj",
+  "Dinsh Kumar",
+  "Gowtham",
+  "Gowri"
+}
+static const char *program_name = "Project GGDB"; //program name itself is 'Project GGDB'
+
+
+//Below are for debugging purposes and to maintain builds only
+static enum build_type {
+  ALPHA,
+  BETA,
+  FINAL
+}
+
+static const int version[] = {ALPHA, 0, 1} //format -> build_type, VERSION_NUMBER, REVISION_NUMBER
+#endif
