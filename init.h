e8d931553f75bd090679c9d71de15fe321702027
Bharatvaj <bharatvaj@yahoo.com>
2017-03-18 14:09:38 +0530

    Update init.h

--- init.h
+++ init.h
@@ -1,6 +1,23 @@
 #ifndef INIT_H
 #define INIT_H
 
+#include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#elif _POSIX_
+#include <unistd.h>
+#endif
+#include "constants.h"
+
+void clear(){
+  #ifdef _WIN32
+  system("cls");
+  #elif _POSIX_
+  system("clear");
+  #endif
+}
+  
+
 void init(){
   std::cout << program_name << " " << version[0] << "-" << version[1] << "." << version[2] << std::endl;
   std::cout << "Program developed by";
