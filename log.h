84ee0b4f51cbeb4945e811e2003be41a3103ced7
Bharatvaj <bharatvaj@yahoo.com>
2017-05-13 13:26:36 +0530

    logging library is made thread safe with use of mutex_locks

--- log.h
+++ log.h
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdarg.h>
-
+#include <pthread.h>
 
 #ifdef __linux__ 
 #define SEPARATOR ('/')
@@ -34,7 +34,9 @@ int *writefd = &n; //for different socket support
 
 char isCol = 0;
 
- static char tag[64] = "";
+pthread_mutex_t lock;
+pthread_mutex_t plock;
+static char tag[64] = "";
 
 enum INIT_OPT {
 	LOG_INIT = 1,
@@ -46,6 +48,8 @@ typedef void (*opt)(int);
 void _opt(int opti){
    if(opti == 1){
 	isCol = 0;
+	pthread_mutex_init(&lock, NULL);
+	pthread_mutex_init(&plock, NULL);
    }
    if(opti == 11){
 	isCol = 1;
@@ -74,15 +78,19 @@ typedef void (*color)(const char *);
 char *colstr = NULL;
 
 void _color(const char *col){
+	pthread_mutex_lock(&plock);
 	printf("%s%s%s\n", col, colstr, COLOR_RESET);
+	pthread_mutex_unlock(&plock);
 }
 
 color print(const char *msg, ...){
 	va_list vl;
+	pthread_mutex_lock(&plock);
 	colstr = (char *)malloc(BUFFER_SIZE);
 	va_start(vl, msg);
 	vsprintf(colstr ,msg, vl);
 	va_end(vl);
+	pthread_mutex_unlock(&plock);
 	return _color;
 }
 
@@ -94,24 +102,29 @@ char * str;
 
 void _level(enum LEVEL log_level){
 #ifdef LOG
+	pthread_mutex_lock(&lock);
 	if(log_level == INFO)
-		printf("%s[!]%s %s: %s\n",isCol == 1?YELLOW:"", isCol == 1?COLOR_RESET:"",tag,str);
+		fprintf(stdout, "%s[!]%s %s: %s\n",isCol == 1?YELLOW:"", isCol == 1?COLOR_RESET:"",tag,str);
 	else if(log_level == ERROR){
 		fprintf(stderr, "[X] %s: %s\n",tag,str);
 	}
 	else if(log_level == PERROR){
+		perror(str);
 	}
 	else if(log_level == CRITICAL)	
-		printf("[CRITICAL] %s: %s\n",tag,str);
+		fprintf(stderr, "[CRITICAL] %s: %s\n",tag,str);
+	pthread_mutex_unlock(&lock);
 #endif
 }
 
 level logger(const char *msg, ...){
 #ifdef LOG
+	pthread_mutex_lock(&lock);
 	str = (char *)malloc(sizeof(msg) + BUFFER_SIZE);
 	va_start(vl, msg);
 	vsprintf(str,msg,vl);
 	va_end(vl);
+	pthread_mutex_unlock(&lock);
 #endif
 	return _level;
 
