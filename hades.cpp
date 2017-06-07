ab2df82bd32619aa1c16f5ea5dbfd2c0b5c36747
System Administrator <root@Bharatvajs-MacBook-Air.local>
2017-06-07 08:11:56 +0530

    Started work for Hades client - Desktop Version

--- hades.cpp
+++ hades.cpp
@@ -1,17 +1,39 @@
 #define HADES_MAIN
 #include <iostream>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include "Fl_OpenCV.H"
+#include <thread>
 
 #define ENABLE_LOG
 #include "logc.h"
 
 const char *TAG = "HADES";
 
-void load_gui(){
-	
+Fl_Window *root = nullptr;
+Fl_OpenCV *open_highgui;
+pthread_t ui_thread;
+
+int start_gui(){
+	root -> show();
+	return Fl::run();
+}
+
+
+void *load_gui(void *opt){
+	root = new Fl_Window(0, 0, 256, 256, "Hades");
+	open_highgui = new Fl_OpenCV(0,0, 128, 128);
+	root->add(open_highgui);
+	start_gui();
+	return nullptr;
 }
 
 int main(int argc, char *argv[]){
-	log_inf(TAG, "starting Daemon");
-	load_gui();
+	std::ios_base::sync_with_stdio(false);
+	log_inf(TAG, "Starting GUI");
+	//pthread_create(&ui_thread, NULL, load_gui, NULL);
+	//pthread_join(ui_thread, NULL);
+	load_gui(nullptr);
+	log_inf(TAG, "lol");
 	return 0;
 }
